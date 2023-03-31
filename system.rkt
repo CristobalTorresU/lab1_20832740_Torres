ab45e66d2e1c18a5e873b8f2aadc15f7330e0957
dilgetto <cristobal.torres.u@usach.cl>
2023-03-31 17:24:17 -0300

    TDA system - run

--- system.rkt
+++ system.rkt
@@ -2,7 +2,13 @@
 ;Implementación del TDA system
 
 ;CONSTRUCTOR
-;descripción: Permite crear un sistema
+;descripción: Permite crear un sistema y registra la fecha de creación
 ;dom: string
 ;rec: lista
 (define system (lambda (name) (list name (seconds->date (current-seconds)))))
+
+;MODIFICADOR
+;descripción: 
+;dom: system x command (función)
+;rec: system
+(define run (lambda (system command) (command system)))
