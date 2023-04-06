081b3845cb59cb1081d2146672859bfb86c92cc1
dilgetto <cristobal.torres.u@usach.cl>
2023-04-05 23:28:29 -0400

    Arreglo función md y TDA folder

--- folder.rkt
+++ folder.rkt
@@ -10,23 +10,30 @@
 ;rec: system
 (define md (lambda (system)
              (lambda (name)
-                   (insertar (list-ref system 0)
+                   (if (equal? #t (comparar_rutas (rutas (cdddar (list-ref system 1)) null) (formar_ruta (cdr (ruta_actual system)) name (car (ruta_actual system)))))
+                       (insertar (list-ref system 0)
                              (append (list (append (car (list-ref system 1))
                                              (list (list (append (list-ref (car system) 4) (list name))))))
                                      (cdr (list-ref system 1)))
-                             (list-ref system 2)))))
+                             (list-ref system 2))
+                       system))))
 
 ;OTRAS OPERACIONES
 ;descripción: 
 ;recursión: 
 ;dom: 
 ;rec: 
-(define formar_ruta (lambda (path name)
-                      (if (null? (cdr path))
-                          (string-append (car path) "/" name "/")
-                          (formar_ruta (list (string-append (car path) (cadr path))
-                                             (if (null? (cddr path)) null (cddr path)))
-                                       name))))
+(define formar_ruta (lambda (path folder_name name)
+                      (if (null? path)
+                          (if (equal? "" folder_name)
+                              (string-append name "/")
+                              (string-append name "/" folder_name "/"))
+                          (formar_ruta (cdr path) folder_name (string-append name "/" (car path))))))
+
+;descripción: 
+;dom: 
+;rec: 
+(define ruta_actual (lambda (system) (list-ref (car system) 4)))
 
 ;MODIFICADOR
 ;descripción: 
@@ -38,3 +45,19 @@
                                (list-ref system 2)
                                (list-ref system 3)
                                path)))
+
+;descripción: 
+;dom: 
+;rec: 
+(define rutas (lambda (carpetas lista)
+                (if (null? carpetas)
+                    lista
+                    (rutas (cdr carpetas) (append lista (list (formar_ruta (cdaar carpetas) "" (caaar carpetas))))))))
+
+;descripción: 
+;dom: 
+;rec: 
+(define comparar_rutas (lambda (rutas path)
+                         (if (null? (filter (lambda (rutas) (equal? rutas path)) rutas))
+                             #t
+                             #f)))
